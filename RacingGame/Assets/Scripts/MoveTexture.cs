2a9405bc989d3ccc8603154fc1e13fce0cbd2265
Petiot Thibaut <petiotthib@gmail.com>
2017-06-24 15:26:23 +0200

    First commit

---
+++ RacingGame/Assets/Scripts/MoveTexture.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveTexture : MonoBehaviour {
+
+    public Vector2 VecSpeed;
+    
+
+    public void Start()
+    {
+        StartCoroutine(Move());
+    }
+
+    public IEnumerator Move()
+    {
+        var mesh = GetComponent<MeshRenderer>();
+        if (mesh != null)
+        {
+            var _mat = mesh.material;
+
+            if (_mat != null)
+            {
+                Vector2 currentOffset = Vector2.zero;
+                Vector2 subValue = new Vector2(0.0f, 1.0f);
+                while (true)
+                {
+                    currentOffset += Time.deltaTime * VecSpeed;
+                    if (currentOffset.y > subValue.y)
+                        currentOffset -= subValue;
+
+                    _mat.SetTextureOffset("_MainTex", currentOffset);
+                    yield return null;
+                }
+            }
+        }
+    }
+}
